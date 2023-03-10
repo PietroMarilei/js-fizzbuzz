fc15758519351dc1430cbce491bbb9fc3b0294c4
PietroMarilei~ <pietromrl0@gmail.com>
2023-03-10 15:28:12 +0100

    it works js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,14 +4,14 @@ for (let number = 1; number <= 100; number++ ) {
      console.log(number);
 
     
-    if (number % 3 == 0 ) {
-        console.log('Fizz');
+    if (number % 3 == 0 && number % 5 == 0 ) {
+        console.log('FizzBuzz');
     
 }   else if (number % 5 == 0 ){
         console.log('Buzz');
 
-}   else if (number % 3 == 0 && number % 5 == 0) {
-        console.log('FizzBuzz');
+}   else if (number % 3 == 0 ) {
+        console.log('Fizz');
 
 }
 

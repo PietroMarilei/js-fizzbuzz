f80cc64da5328ecf2a575eccdab34d6aaf75e643
PietroMarilei~ <pietromrl0@gmail.com>
2023-03-10 15:15:25 +0100

    first attempt

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+// stampo in console i numeri da 1 a 100
+
+for (let number = 1; number <= 100; number++ ) {
+     console.log(number);
+
+     if (number % 3 == 0) {
+        console.log('divisibile per 3');
+    
+} else if (number % 5 == 0 ){
+        console.log('divisibile per 5');
+
+} else if (number % 3 == 0 && number % 5 == 0) {
+        console.log('bla bla');
+
+}
+
+}
+// se multipli di 3 stampi fizz
+

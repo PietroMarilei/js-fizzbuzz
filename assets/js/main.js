0a5c3d2710d91b905353f21ace8f9133638565ec
PietroMarilei~ <pietromrl0@gmail.com>
2023-03-10 16:28:39 +0100

    need to fix line 31

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,11 +28,13 @@ for (let number = 1; number <= 100; number++ ) {
     
     if (number % 3 == 0 && number % 5 == 0 ) {    
         console.log('FizzBuzz');
+        document.querySelector('li').innerHTML = 'FizzBuzz';
 
         
     
 }   else if (number % 5 == 0 ){
         console.log('Buzz');
+        
 
 }   else if (number % 3 == 0 ) {
         console.log('Fizz');

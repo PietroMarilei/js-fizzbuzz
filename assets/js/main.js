721e9ce8ba020d3cbc623a3e005198f9d175d4c1
PietroMarilei~ <pietromrl0@gmail.com>
2023-03-10 16:19:32 +0100

    add some squares

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,35 @@
 // stampo in console i numeri da 1 a 100
 
+// for (let number = 1; number <= 100; number++ ) {
+//      console.log(number);
+
+    
+//     if (number % 3 == 0 && number % 5 == 0 ) {
+//         console.log('FizzBuzz');
+    
+// }   else if (number % 5 == 0 ){
+//         console.log('Buzz');
+
+// }   else if (number % 3 == 0 ) {
+//         console.log('Fizz');
+
+// }
+
+// }
+
+
 for (let number = 1; number <= 100; number++ ) {
      console.log(number);
 
+     const liElement = document.createElement ('li');
+        liElement.append(number);
+        document.querySelector('ul').append(liElement);
+
     
-    if (number % 3 == 0 && number % 5 == 0 ) {
+    if (number % 3 == 0 && number % 5 == 0 ) {    
         console.log('FizzBuzz');
+
+        
     
 }   else if (number % 5 == 0 ){
         console.log('Buzz');
@@ -16,5 +40,3 @@ for (let number = 1; number <= 100; number++ ) {
 }
 
 }
-
-

5a458d5ebce86fa5866a54271623b4b9635898f7
PietroMarilei~ <pietromrl0@gmail.com>
2023-03-10 17:09:19 +0100

    add some style and fix name generator

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,10 @@
 // for (let number = 1; number <= 100; number++ ) {
 //      console.log(number);
 
-    
+
 //     if (number % 3 == 0 && number % 5 == 0 ) {
 //         console.log('FizzBuzz');
-    
+
 // }   else if (number % 5 == 0 ){
 //         console.log('Buzz');
 
@@ -18,27 +18,34 @@
 // }
 
 
-for (let number = 1; number <= 100; number++ ) {
-     console.log(number);
+for (let number = 1; number <= 100; number++) {
+        console.log(number);
 
-     const liElement = document.createElement ('li');
-        liElement.append(number);
+        const liElement = document.createElement('li');
+        liElement.innerHTML = number;
+
+
+        if (number % 3 == 0 && number % 5 == 0) {
+                console.log('FizzBuzz');
+                liElement.innerHTML = 'FizzBuzz';
+                liElement.classList.add('bg-warning', 'fw-bold', 'text-danger');
+
+
+
+        } else if (number % 5 == 0) {
+                console.log('Buzz');
+                liElement.innerHTML = 'Buzz';
+                liElement.classList.add('bg-danger', 'fw-bold');
+
+
+        } else if (number % 3 == 0) {
+                console.log('Fizz');
+                liElement.innerHTML = 'Fizz';
+                liElement.classList.add('bg-success', 'fw-bold', 'text-warning');
+
+
+        }
+
         document.querySelector('ul').append(liElement);
-
-    
-    if (number % 3 == 0 && number % 5 == 0 ) {    
-        console.log('FizzBuzz');
-        document.querySelector('li').innerHTML = 'FizzBuzz';
-
-        
-    
-}   else if (number % 5 == 0 ){
-        console.log('Buzz');
-        
-
-}   else if (number % 3 == 0 ) {
-        console.log('Fizz');
-
-}
 
 }
